2f1230557b5fe50df1000e6790cac54c584cdaa7
kimsm28 <sm28.kim@samsung.com>
2025-03-26 17:36:11 +0900

    test

--- pkg/service/alerting/alerting.go
+++ pkg/service/alerting/alerting.go
@@ -319,7 +319,7 @@ func getDataFromLogs(bodyBytes []byte) ([]commonmodel.Sample, error) {
 		return nil, nil
 	}
 	var logData commonmodel.Metric
-	logData["log"] = string(bodyBytes)
+	logData["log"] = commonmodel.LavelValue(string(bodyBytes))
 
 	return []commonmodel.Sample{{
 		Metric: logData,
